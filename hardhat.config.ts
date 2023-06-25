773f7ab732b1470277fb97cf3b7530bff9e83c82
williamyang-zzu <1654263161@qq.com>
2023-06-25 23:50:40 +0800

    learn Hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,32 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
+};
+
+export default config;
+
+require("@nomicfoundation/hardhat-toolbox");
+
+// Go to https://infura.io, sign up, create a new API key
+// in its dashboard, and replace "KEY" with it
+const INFURA_API_KEY = "KEY";
+
+// Replace this private key with your Goerli account private key
+// To export your private key from Coinbase Wallet, go to
+// Settings > Developer Settings > Show private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const GOERLI_PRIVATE_KEY = "GOERLI_PRIVATE_KEY";
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  }
+};
